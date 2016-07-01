6c8b5a6eba5429823a004dde3dd8cfdbe5b3a395
toshiro <tsubasa.oshiro@gmail.com>
2016-07-01 23:54:46 +0900

    タイトル、説明の出力

--- itunesAPI.php
+++ itunesAPI.php
@@ -2,7 +2,7 @@
 
 $base_url = 'https://itunes.apple.com/search';
 
-$term = rawurlencode('すくみず');
+$term = rawurlencode('脱出ゲーム');
 $country = 'jp';
 
 $media = 'software';
@@ -16,8 +16,29 @@ $limit = 10;
 $offset = 0;
 $lang = 'ja_jp';
 
-$request = $base_url.'?term='.$term.'&media='.$media.'&entity='.$entity.'&country='.$country
+$url = $base_url.'?term='.$term.'&media='.$media.'&entity='.$entity.'&country='.$country
 			.'&lang='.$lang.'&limit='.$limit;
 
-echo $request;
+// REST リクエストの発行
+// require_once('HTTP/Request.php');
+// $request = new HTTP_Request($url);
+// $result = $request->sendRequest();
 
+// // レスポンスを取得
+// $json = $request->getResponseBody();
+
+// // 連想配列に格納
+// $data = json_decode($json, true);
+
+
+// PHP5
+$json = file_get_contents($url);
+$data = json_decode($json, true);
+
+foreach ($data['results'] as $row) {
+	$title = $row['trackCensoredName'];
+	$description = $row['description'];
+
+	echo "<div><h1>".$title."</h1>";
+	echo nl2br( htmlspecialchars($description) )."</div>";
+}
